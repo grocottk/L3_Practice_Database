559ab29d36f470561ad3181e0f3005c49aa3cb09
Kahlil <51727305+grocottk@users.noreply.github.com>
2021-08-11 13:17:26 +1200

    Build before Show All Entries
    This build is mostly functional and has been made before the Show All Entries page.

--- L3_Site_Template/index.php
+++ L3_Site_Template/index.php
@@ -12,7 +12,19 @@
         
         <div class="box main">
 
-            <?php include("content/home.php"); ?>
+            <?php
+
+            if(!isset($_REQUEST['page'])) {
+                include("content/home.php");
+            } // This is the end that includes the home page
+
+            else {
+                // This prevents users from navigating through [a] file system
+                $page=preg_replace('/[^0-9a-zA-Z]-/','',$_REQUEST['page']);
+                include("content/$page.php");
+            } // This is the end that includes the user's requested content
+
+            ?>
                     
         </div>    <!-- / main -->
         
