d0aef9f6763d0163c7380f52ab03c84970938eac
Kahlil <51727305+grocottk@users.noreply.github.com>
2021-08-12 12:29:28 +1200

    Build during Show All Part 3 video
    This is a build of the code during the third part of the Show All series

--- L3_Site_Template/content/showall.php
+++ L3_Site_Template/content/showall.php
@@ -8,4 +8,30 @@ JOIN Author ON (`Author`.`Author_ID` = `Quotes`.`Author_ID`)
 $find_query = mysqli_query($dbconnect, $find_sql);
 $find_rs = mysqli_fetch_assoc($find_query);
 
+// Loop through results and display them...
+do {
+
+    $quote = preg_replace('/[^A-Za-z0-9.,?\s\'\-]/', ' ', $find_rs['Quote']); // From ""[...]useful_code.txt""
+
+    // Author name...
+    $first = $find_rs['First_Name'];
+    $middle = $find_rs['Middle_Name'];
+    $last = $find_rs['Last_Name'];
+
+    $full_name = $first." ".$middle." ".$last;
+
+    ?>
+<div class="results">
+    <?php echo $quote; ?><br />
+    <a href="index.php?page=author&Author_ID=<?php echo $find_rs['Author_ID']; ?>">
+    <?php echo $full_name; ?>
+    </a>
+</div>
+<br />
+<?php
+
+} // End of the do command for display results
+
+while($find_rs = mysqli_fetch_assoc($find_query))
+
 ?>
