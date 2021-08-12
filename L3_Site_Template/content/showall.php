4cb7094b525fe7c49e1d3e50329d456f9ddb92f6
Kahlil <51727305+grocottk@users.noreply.github.com>
2021-08-13 11:04:23 +1200

    Build during Show All Part 4 video
    This is a build taken during the development of the program in terms of the Show All Part 4 video.

--- L3_Site_Template/content/showall.php
+++ L3_Site_Template/content/showall.php
@@ -22,10 +22,35 @@ do {
 
     ?>
 <div class="results">
-    <?php echo $quote; ?><br />
-    <a href="index.php?page=author&Author_ID=<?php echo $find_rs['Author_ID']; ?>">
-    <?php echo $full_name; ?>
-    </a>
+    <p>
+        <?php echo $quote; ?><br />
+        <a href="index.php?page=author&Author_ID=<?php echo $find_rs['Author_ID']; ?>">
+        <?php echo $full_name; ?>
+        </a> <!-- Author name -->
+    </p>
+
+    <!-- Subject tags to go here -->
+    <p>
+        <?php
+            $Subject_1_ID = $find_rs['Subject_1_ID'];
+            $Subject_2_ID = $find_rs['Subject_2_ID'];
+            $Subject_3_ID = $find_rs['Subject_3_ID'];
+
+            $all_subjects = array($Subject_1_ID, $Subject_2_ID, $Subject_3_ID);
+
+            // Loops through the subject IDs and looks for the subject name
+            foreach($all_subjects as $subject) {
+                // Gets subject name
+                $sub_sql = "SELECT * FROM `Subjects` WHERE `Subject_ID` = $subject";
+                $sub_query = mysqli_query($dbconnect, $sub_sql);
+                $sub_rs = mysqli_fetch_assoc($sub_query);
+            } // The end of the subject loop
+            ?>
+        <!-- Show subjects -->
+        <span class="tag">
+                <?php echo $sub_rs['Subject']; ?>
+        </span>
+    </p>
 </div>
 <br />
 <?php
