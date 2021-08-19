23faebd6f207bcf38d7982f836cca0613c95d9b2
Kahlil <51727305+grocottk@users.noreply.github.com>
2021-08-19 20:11:56 +1200

    Build with GK Files included

---
+++ GK Files/showall.php
@@ -0,0 +1,33 @@
+<h2>All Results</h2>
+
+<?php
+
+$find_sql = "SELECT * FROM `Quotes`
+JOIN Author ON (`Author`.`Author_ID` = `Quotes`.`Author_ID`)
+";
+$find_query = mysqli_query($dbconnect, $find_sql);
+$find_rs = mysqli_fetch_assoc($find_query);
+
+// Loop through results and display them...
+do {
+    $quote = preg_replace('/[^A-Za-z0-9.,?\s\'\-]/', ' ', $find_rs['Quote']); // From ""[...]useful_code.txt""
+    include("get_author.php");
+    ?>
+<div class="results">
+    <p>
+        <?php echo $quote; ?><br />
+        <!-- Displays author name -->
+        <a href="index.php?page=author&Author_ID=<?php echo $find_rs['Author_ID']; ?>">
+        <?php echo $full_name; ?>
+        </a> <!-- Author name -->
+    </p>
+    <?php include("show_subjects.php"); ?>
+</div>
+<br />
+<?php
+
+} // End of the do command for display results
+
+while($find_rs = mysqli_fetch_assoc($find_query))
+
+?>
