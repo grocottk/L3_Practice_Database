29e80fb20f1f897bc4bb56b20ac14a5ff91748fe
Kahlil <51727305+grocottk@users.noreply.github.com>
2021-09-01 13:33:17 +1200

    Build during New Quote Part 2 video

--- L3_Site_Template/admin/new_quote.php
+++ L3_Site_Template/admin/new_quote.php
@@ -4,6 +4,17 @@
 // Checks that the user is logged in...
 if (isset($_SESSION['admin'])) {
     echo "you are logged in";
+
+    // Gets authors from database
+    $all_authors_sql = "SELECT * FROM `author` ORDER BY `author`.`Last_Name` ASC";
+    $all_authors_query = mysqli_query($dbconnect, $all_authors_sql);
+    $all_authors_rs = mysqli_fetch_assoc($all_authors_query);
+
+    // Initialises author form
+    $first = "";
+    $middle = "";
+    $last = "";
+
 } // End of user logged in if statement
 
 else {
@@ -12,3 +23,14 @@ else {
 } // End of user logged in else statement
 
 ?>
+
+<h1>Add a quote</h1>
+<p><i>
+    To add a quote, first select the author, then press the 'next' button.
+</i></p>
+
+<form method="post" enctype="multipart/form-data" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]."?page=../admin/new_quote");?>"> <!-- Sourced from support files (possibly originally from W3 Schools) -->
+    <div>
+        <b>Quote Author:</b> &nbsp;
+    </div>
+</form>
