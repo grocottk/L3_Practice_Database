c8fef8e92c80de3647d7da3fe38a6b1a5489c256
Kahlil <51727305+grocottk@users.noreply.github.com>
2021-09-15 13:31:09 +1200

    Build during Delete Quotes & Authors video

--- L3_Site_Template/admin/deletequote.php
+++ L3_Site_Template/admin/deletequote.php
@@ -4,6 +4,19 @@
 // Checks that the user is logged in...
 if (isset($_SESSION['admin'])) {
     echo "you are logged in";
+
+    // Quote deleting (inspired by "[...]deletequote_confirm.php")
+    $deletequote_sql = "DELETE FROM `quotes` WHERE `quotes`.`ID` =".$_REQUEST['ID'];
+    $deletequote_query = mysqli_query($dbconnect, $deletequote_sql);
+
+?>
+
+<h1>Delete Success</h1>
+
+<p>The quote has been deleted</p>
+
+<?php
+
 } // End of user logged in if statement
 
 else {
